d6f369374212827c0081c820f2fb247933eff61c
Wassim SAMAD <wass08@gmail.com>
2022-02-18 19:12:28 +0100

    remove test

--- src/screens/Product/Products.tsx
+++ src/screens/Product/Products.tsx
@@ -3,7 +3,6 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { tailwind } from "tailwind";
-import { useProductsListQuery } from "./Products.generated";
 
 interface ProductsProps {}
 
@@ -11,9 +10,7 @@ export const Products: React.FunctionComponent<ProductsProps> = ({}) => {
   const buttonPressed = () => {
     console.log("Button pressed");
   };
-  const { data } = useProductsListQuery();
 
-  console.log(data);
   return (
     <SafeAreaView style={tailwind("flex flex-1")}>
       <View style={tailwind("flex-1")}></View>
